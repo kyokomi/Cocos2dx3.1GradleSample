b32471fea2413e2903dbcd2179224fd0712a15c8
kyokomi <kyoko1220adword@gmail.com>
2014-05-23 22:27:02 +0900

    gradle対応

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+COCOS2DX_ROOT := $(LOCAL_PATH)/../../cocos2d
 
 include $(CLEAR_VARS)
 
@@ -6,31 +7,46 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+#cppファイルの自動検索設定
+ifeq ($(HOST_OS),windows)
+	CPP_FILES := $(shell dir $(LOCAL_PATH)/../../Classes/*.cpp /b/a-d/s)                   
+else
+	CPP_FILES := $(shell find $(LOCAL_PATH)/../../Classes -name *.cpp)                   
+endif
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
+
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+#headerフォルダの自動検索設定
+ifeq ($(HOST_OS),windows)
+	LOCAL_C_INCLUDES := $(shell dir $(LOCAL_PATH)/../../Classes /b/ad/s)                   
+else
+	LOCAL_C_INCLUDES := $(shell find $(LOCAL_PATH)/../../Classes -type d)                   
+endif
+
+ifeq ($(APP_OPTIM),debug)
+	LOCAL_CFLAGS := -w -g
+endif
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dxandroid_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
-
-# LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
-
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 
 include $(BUILD_SHARED_LIBRARY)
 
+$(call import-add-path,$(COCOS2DX_ROOT))
+$(call import-add-path,$(COCOS2DX_ROOT)/cocos)
+$(call import-add-path,$(COCOS2DX_ROOT)/external)
+$(call import-add-path,$(COCOS2DX_ROOT)/cocos/platform/android)
+
 $(call import-module,.)
 $(call import-module,audio/android)
+$(call import-module,editor-support/spine)
+$(call import-module,editor-support/cocostudio)
 
-# $(call import-module,Box2D)
-# $(call import-module,editor-support/cocosbuilder)
-# $(call import-module,editor-support/spine)
-# $(call import-module,editor-support/cocostudio)
-# $(call import-module,network)
-# $(call import-module,extensions)
+$(call import-module,platform/android)
+
--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,2 +1,8 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -DCOCOS2D_DEBUG=1 -std=c++11 -fsigned-char
+
+APP_ABI :=armeabi
+APP_OPTIM := debug
+APP_PLATFORM := android-9
+
+NDK_TOOLCHAIN_VERSION=4.8
